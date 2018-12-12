1253695e4b0b8146821aa9f9dd88629b1472b7df
Saad Mazhar <42980243+notsaad@users.noreply.github.com>
2018-12-12 15:10:34 -0500

    Create platformer.js

---
+++ platformer.js
@@ -0,0 +1,52 @@
+/*
+var collision = function(r1, r2) {
+  if (r1.x + r1.w > r2.x &&
+      r1.x < r2.x + r2.w &&
+      r2.y + r2.h > r1.y &&
+      r2.y < r1.y + r1.h) {
+        return true;
+  } else {
+    return false;
+  }
+};
+
+var placeFree = function(xNew, yNew) {
+	var temp = { x: xNew, y: yNew, w: player.w, h: player.h};
+	
+}
+
+var up = 0;
+var down  = 0;
+var left = 0;
+var right = 0;
+
+var keyPressed = function() {
+  if (keyCode === 'ArrowUp') {
+    up = 1;
+  }
+  if (keyCode === 'ArrowDown') {
+    down = 1;
+  }
+  if (keyCode === 'ArrowLeft') {
+    left = 1;
+  }
+  if (keyCode === 'ArrowRight') {
+    right = 1;
+  }
+};
+
+var keyReleased = function() {
+  if (keyCode === 'ArrowUp') {
+    up = 0;
+  }
+  if (keyCode === 'ArrowDown') {
+    down = 0;
+  }
+  if (keyCode === 'ArrowLeft') {
+    left = 0;
+  }
+  if (keyCode === 'ArrowRight') {
+    right = 0;
+  }
+};
+*/
