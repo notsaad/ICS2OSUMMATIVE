0db3f8c79bc5ab26fa679a7d62707969ae43b3b2
Saad Mazhar <42980243+notsaad@users.noreply.github.com>
2019-01-10 14:01:08 -0500

    platform and coin objects

---
+++ platform.js
@@ -0,0 +1,25 @@
+function platform (x,y,width,height){
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height  =  height;
+
+
+this.show = function(){
+    fill(0);
+  	rect(this.x, this.y, this.width, this.height);
+}
+}
+
+function coin (x,y) {
+  this.x = x;
+  this.y = y;
+  
+  this.show = function(){
+    fill(255, 240, 93);
+    ellipse(this.x, this.y, 40, 40);
+		fill(0);
+    textSize(12);
+    text("( ͡° ͜ʖ ͡°)", this.x, this.y + 5);
+    }
+    }
