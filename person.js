bdc20e36254755030cd331d6d85cd5bbded85a58
Saad Mazhar <42980243+notsaad@users.noreply.github.com>
2019-01-10 14:01:42 -0500

    collision mechanics

--- person.js
+++ person.js
@@ -1,18 +1,19 @@
 function Person() {
-  this.pos = createVector(50, height, 20, 50);
+  this.pos = createVector(50, height , 20, 50);
+  // this.oldpos = createVector(50 )
   this.vel = createVector(0*0.95, 0);
   this.acc = createVector(0, 0);
-	
+
   this.applyForce = function(force) {
     this.acc.add(force);
   }
-	
+
   this.update = function() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
-  
+
   this.display = function() {
   	if(jumping == true) {
     fill(random(255), random(255), random(255));
@@ -22,47 +23,42 @@ function Person() {
     noStroke();
     rect(this.pos.x, this.pos.y - 50, 20, 50);
   }
-  
-  this.edges = function() {
-    if (this.pos.y > height) {
+
+  this.edges = function(ground) {
+  if (this.pos.x > ground.x && this.pos.x < ground.x + ground.width) {
+    
+    if (this.pos.y > ground.y) {
       this.vel.y *= 0;
-      this.pos.y = height;
-      
-         if(this.pos.y >= height){
-     
+      this.pos.y = ground.y;
+
+         if(this.pos.y >= ground.x){
+
       		jumping = false;
           this.vel.x = 0;
-      
-    	}
-      
-      if (this.pos.x > width + 10) {
-       
-        this.pos.x = width - 10;
-        
-      }
-      
+         }
+    } 
+
       if (this.pos.x < -10) {
-       
+
         this.pos.x = -10;
-        
+
       }
-    }
-	} 
+
+    }    else {
+      if (this.pos.y > height) {
+        this.vel.x *=0;
+        this.pos.y = height;
+        	 
+        	if(this.pos.y >= height){
+           jumping = false;
+            this.vel.x = 0;
+          }
+      }
+      if (this.pos.x > width + 10) {
+
+        this.pos.x = width - 10;
+
+      }
+	}
 }
-
-/*class Coin {
-  constructor(pos, basePos, wobble) {
-    this.pos = pos;
-    this.basePos = basePos;
-    this.wobble = wobble;
-  }
-
-  get type() { return "coin"; }
-
-  static create(pos) {
-    let basePos = pos.plus(new Vec(0.2, 0.1));
-    return new Coin(basePos, basePos,
-                    Math.random() * Math.PI * 2);
-  }
 }
-*/
