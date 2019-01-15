f2f8cfe516393fcdd75c9420e6ca75e31fc167c1
Saad Mazhar <42980243+notsaad@users.noreply.github.com>
2019-01-15 14:36:02 -0500

    Update person.js

--- person.js
+++ person.js
@@ -1,6 +1,5 @@
 function Person() {
   this.pos = createVector(50, height , 20, 50);
-  // this.oldpos = createVector(50 )
   this.vel = createVector(0*0.95, 0);
   this.acc = createVector(0, 0);
 
@@ -39,26 +38,127 @@ function Person() {
     } 
 
       if (this.pos.x < -10) {
-
         this.pos.x = -10;
-
       }
-
-    }    else {
+    } else {
       if (this.pos.y > height) {
-        this.vel.x *=0;
-        this.pos.y = height;
-        	 
-        	if(this.pos.y >= height){
-           jumping = false;
-            this.vel.x = 0;
-          }
+        	this.vel.x *=0;
+        	this.pos.y = height;
+        
+      if (this.pos.y >= height){
+        	jumping = false;
+          this.vel.x = 0;
+      }
       }
       if (this.pos.x > width + 10) {
 
         this.pos.x = width - 10;
-
-      }
+			}
+		}
 	}
-}
+  
+    this.edges2 = function(ground2) {
+  if (this.pos.x > ground2.x && this.pos.x < ground2.x + ground2.width) {
+    
+    if (this.pos.y > ground2.y) {
+      this.vel.y *= 0;
+      this.pos.y = ground2.y;
+
+         if(this.pos.y >= ground2.x){
+
+      		jumping = false;
+          this.vel.x = 0;
+         }
+    } 
+
+      if (this.pos.x < -10) {
+        this.pos.x = -10;
+      }
+    } else {
+      if (this.pos.y > height) {
+        	this.vel.x *=0;
+        	this.pos.y = height;
+        
+      if (this.pos.y >= height){
+        	jumping = false;
+          this.vel.x = 0;
+      }
+      }
+      if (this.pos.x > width + 10) {
+
+        this.pos.x = width - 10;
+			}
+		}
+	}
+  
+    this.edges3 = function(ground3) {
+  if (this.pos.x > ground3.x && this.pos.x < ground3.x + ground3.width) {
+    
+    if (this.pos.y > ground3.y) {
+      this.vel.y *= 0;
+      this.pos.y = ground3.y;
+
+         if(this.pos.y >= ground3.x){
+
+      		jumping = false;
+          this.vel.x = 0;
+         }
+    } 
+
+      if (this.pos.x < -10) {
+        this.pos.x = -10;
+      }
+    } else {
+      if (this.pos.y > height) {
+        	this.vel.x *=0;
+        	this.pos.y = height;
+        
+      if (this.pos.y >= height){
+        	jumping = false;
+          this.vel.x = 0;
+      }
+      }
+      if (this.pos.x > width + 10) {
+
+        this.pos.x = width - 10;
+			}
+		}
+	}
+    
+    this.edges4 = function(platform1) {
+  if (this.pos.x > platform1.x && this.pos.x < platform1.x + platform1.width) {
+    if (this.pos.y <= platform1.y) {
+      this.vel.y *= 0;
+      this.pos.y = platform1.y;
+
+    } 
+
+      if (this.pos.x < -10) {
+        this.pos.x = -10;
+      }
+    } else {
+      if (this.pos.y > height) {
+        	this.vel.x *=0;
+        	this.pos.y = height;
+        
+      if (this.pos.y >= height){
+        	jumping = false;
+          this.vel.x = 0;
+      }
+      }
+      if (this.pos.x > width + 10) {
+
+        this.pos.x = width - 10;
+			}
+		}
+	}
+    
+    this.death = function(lava1) {
+  if (this.pos.x + 20 > lava1.x && this.pos.x < lava1.x + lava1.width &&
+      this.pos.y > lava1.y && this.pos.y < lava1.y + lava1.height){
+    	life -= 1;
+    	this.pos.x = 50;
+  	}
+	}
+    
 }
